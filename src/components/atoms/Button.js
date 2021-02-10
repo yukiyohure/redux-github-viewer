d37f934301c355e9672e794f9f8635c6631b7379
yukiyohure <yukiyohure@gmail.com>
2021-02-10 21:09:38 +0900

    use jsx

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -52,8 +52,8 @@ const buttonStyleLists = {
 };
 
 const Button = ({ styleType, label, onClick }) => {
-  const component = buttonStyleLists[styleType] || buttonStyleLists.default;
-  return React.createElement(component, { onClick }, label);
+  const Component = buttonStyleLists[styleType] || buttonStyleLists.default;
+  return <Component onClick={onClick}>{label}</Component>
 };
 
 Button.propTypes = {
